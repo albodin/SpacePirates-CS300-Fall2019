0b8a30a0b1e70c00ec9a048477e45ec98887574b
albodin <abodin@pdx.edu>
2019-11-10 14:50:33 -0800

    Added play type, still need to be able to reset the game to finish the checkSupplies function

--- RunningOutOfSupplies.js
+++ RunningOutOfSupplies.js
@@ -1,4 +1,11 @@
 function checkSupplies() {
-   if (supplies <= 0)
-      alert("You ran out of supplies. Game over");
+   if (supplies <= 0) {
+      if (isRegularPlay) {
+         alert("You ran out of supplies. Game over");
+         //TODO: Call function to reset game
+      }
+      else
+         alert("You ran out of supplies");
+   }
+      
 }
--- globals.js
+++ globals.js
@@ -3,3 +3,4 @@
 var supplies = 0;
 var energy = 0;
 var shipLocation = {x: 0, y: 0};
+var isRegularPlay = 0;
