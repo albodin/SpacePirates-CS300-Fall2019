5d3554c43d54c9de7da019bdf590ac8205011a8f
nelhwong <47463908+nelhwong@users.noreply.github.com>
2019-11-12 11:16:41 -0800

    added spacecraft movement

--- SpaceCraftMovement.js
+++ SpaceCraftMovement.js
@@ -1,5 +1,37 @@
-function moveSpacecraft(angle,distance) {
-   alert("Move Space Craft");
-   checkEnergy();
-   checkSupplies();
+
+function moveSpacecraft(angle, distance) {
+  var distance = document.getElementById("distance").value
+  // alert("Move Space Craft");
+   movement(angle, distance)
+   //checkEnergy();
+  // checkSupplies();
 }
+
+// function checkBoundry(distance) { }
+
+let movement = (angle, distance) => {
+   /*
+      use: moves the spaceshipt based on input index.html
+      */
+   console.log(distance)
+   distance = distance ? distance : 1;
+   //  Up
+   if (angle === 0) {
+      player.position.x += distance;
+   }
+   // Right
+   else if (angle === 90) {
+      player.position.y += distance;
+   }
+   // Left
+   else if (angle === 180) {
+      player.position.y -= distance;
+   }
+   // Down
+   else if (angle === 270) {
+      player.position.x -= distance;
+   }
+
+   console.log(player.position)
+
+};
