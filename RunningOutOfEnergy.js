a68e4e64594cbec304301a30c23f317cce433314
knzgage <40836138+knzgage@users.noreply.github.com>
2019-11-12 13:09:56 -0800

    Update RunningOutOfEnergy.js
    Updated checkEnergy function

--- RunningOutOfEnergy.js
+++ RunningOutOfEnergy.js
@@ -1,3 +1,15 @@
 function checkEnergy() {
    alert ("Checking Energy");
+   if(energy <=0)
+   {
+      if(isRegularPlay)
+      {
+         alert ("You are out of energy. Game over");
+         //TODO restart game
+      }
+
+      else //not regular game mode
+          alert("You ran out of energy")
+
+   }
 }
