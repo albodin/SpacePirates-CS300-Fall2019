dd2a1a1da1002d7e83b034aa1c10fb45b72d3ab6
albodin <abodin@pdx.edu>
2019-11-13 18:13:54 -0800

    checkSupplies now calls restart

--- RunningOutOfSupplies.js
+++ RunningOutOfSupplies.js
@@ -2,7 +2,7 @@ function checkSupplies() {
    if (supplies <= 0) {
       if (isRegularPlay) {
          alert("You ran out of supplies. Game over");
-         //TODO: Call function to reset game
+         restart();
       }
       else
          alert("You ran out of supplies");
