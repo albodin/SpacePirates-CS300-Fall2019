be8d04d5955da6431175b95c1c099a19a23520f4
knzgage <40836138+knzgage@users.noreply.github.com>
2019-11-13 20:23:43 -0800

    Restart function updateds values now in additiion to fields

--- RestartGame.js
+++ RestartGame.js
@@ -1,7 +1,12 @@
+//resets all the values to their start values
 function restart() {
     //TODO test restart function
+    energy = startEnergy;
     document.getElementById("energy").value = startEnergy;
+    supplies = startSupplies;
     document.getElementById("supplies").value = startSupplies;
+    player.position.x = startx;
     document.getElementById("xVal").value = startx;
+    player.position.y = startx;
     document.getElementById("yVal").value = starty;
 }
