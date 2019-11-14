9e87ec1f0c2f878b6fb436cf254def2bee93290a
knzgage <40836138+knzgage@users.noreply.github.com>
2019-11-13 17:25:05 -0800

    Update RunningOutOfEnergy.js
    Updated checkEnergy function so that it calls restart when energy drops below zero and regular play is specified.  The if statement in checkEnergy is not being ran when expected, will need to look into it.
    Also added a function to update the energy field based of off the distance moved.

--- RunningOutOfEnergy.js
+++ RunningOutOfEnergy.js
@@ -1,15 +1,36 @@
+
 function checkEnergy() {
    alert ("Checking Energy");
    if(energy <=0)
    {
-      if(isRegularPlay)
+      //TODO Make sure that is Regular play works correctly
+      if(isRegularPlay === 1)
       {
          alert ("You are out of energy. Game over");
-         //TODO restart game
+         restart();
       }
 
       else //not regular game mode
-          alert("You ran out of energy")
+          alert("You ran out of energy");
 
    }
+   console.log(isRegularPlay)
+}
+
+//updates the energy value after a
+function  updateEnergy(){
+
+   var newEnergy = parseInt(document.getElementById("energy").value);
+   var distMoved = parseInt(document.getElementById("distance").value);
+
+   //if we get a negative or 0 value, assume 1
+   if(distMoved <= 0){
+      distMoved =1
+   }
+   //else
+   //otherwise the distance will remain the same as the input value
+
+   newEnergy -= distMoved;
+   energy = newEnergy;
+   document.getElementById("energy").value = energy;
 }
