4fb9ba0281c131b6b182db08776e68323670e33a
knzgage <40836138+knzgage@users.noreply.github.com>
2019-11-13 17:22:41 -0800

    Created a restart function
    Function sets fields to their start values from the global file

---
+++ RestartGame.js
@@ -0,0 +1,7 @@
+function restart() {
+    //TODO test restart function
+    document.getElementById("energy").value = startEnergy;
+    document.getElementById("supplies").value = startSupplies;
+    document.getElementById("xVal").value = startx;
+    document.getElementById("yVal").value = starty;
+}
